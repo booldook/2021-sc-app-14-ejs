02ac032e3daabe926aed84b4277243471a15c8e9
booldook <booldook@gmail.com>
2021-08-30 16:03:17 +0900

    menu.ejs

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 /*************** global init **************/
-const port = 3000
 const express = require('express')
 const app = express()
-
+const port = 3000
 
 /************** view engine ***************/
 app.set('view engine', 'ejs')
@@ -16,7 +15,16 @@ app.use(express.urlencoded({ extended: false }))
 
 /*************** router init **************/
 app.use('/', express.static('./public'))
-app.get('/', (req, res, next) => {})
+app.get('/coffee', (req, res, next) => {
+	const menus = [
+		{ name: '아메리카노', price: 2500 },
+		{ name: '카페라떼', price: 3500 },
+		{ name: '바닐라라떼', price: 3800 },
+		{ name: '카라멜 마끼아또', price: 4500 },
+	]
+	// res.status(200).render(view파일, {})
+	res.status(200).render('menu', { menus })
+})
 
 
 /*************** server init **************/
