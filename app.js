1987ea872255863dfc2bbf1d3b5ffdb33ea335c5
booldook <booldook@gmail.com>
2021-08-31 11:09:54 +0900

    ejs 구조 완성

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/about', (req, res, next) => {
 	const title = 'About Me'
 	const file = 'about'
 	const content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus asperiores, ratione sed quam aliquid suscipit doloremque, voluptatum reprehenderit, pariatur id officiis! Quam dolore consectetur voluptatibus laudantium nisi repudiandae, quasi quas.'
-	res.render('about', { title, file, content })
+	res.render('site/about', { title, file, content })
 })
 
 app.get('/pf', (req, res, next) => {
@@ -31,7 +31,7 @@ app.get('/pf', (req, res, next) => {
 		{ title: 'PF_02', src: 'f2.jpg' },
 		{ title: 'PF_03', src: 'f3.jpg' },
 	]
-	res.render('about', { title, file, content })
+	res.render('site/pf', { title, file, lists })
 })
 
 
